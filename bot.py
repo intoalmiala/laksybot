dd6951571d334cc39ff4d597727d55a0d56e47c1
NoSocksForYou <33051516+NoSocksForYou@users.noreply.github.com>
2017-11-05 18:13:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 def watson(text): #This function deternies, which school subject is being talked about in the input string.
     natural_language_classifier = NaturalLanguageClassifierV1(
         username='5b314083-6286-4ad6-86b0-c6d0ea4aa266',
-        password='mawh434bmDVG')
+        password='jotain tosi salaista')
 
     response = natural_language_classifier.classify('1e0d8ex232-nlc-26798', text)['top_class']
     return(str(response))
