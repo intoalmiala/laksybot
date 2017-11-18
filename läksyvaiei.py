e3ebe89cd46e8f88b6a9e443be18ac6105889038
NoSocksForYou <33051516+NoSocksForYou@users.noreply.github.com>
2017-11-18 11:02:28 +0200

    Update läksyvaiei.py

--- läksyvaiei.py
+++ läksyvaiei.py
@@ -8,7 +8,11 @@ import json
 from os.path import join, dirname
 from os import environ
 from watson_developer_cloud import VisualRecognitionV3
-
+params =  { "classifier_ids": ["liitutauluvaiei_1285787542"] }
 visual_recognition = VisualRecognitionV3('2016-05-20', api_key='d51997e99c32eeea53de579fa1337829f6c1c3b8')
 
-print(json.dumps(visual_recognition.classify(images_url='https://www.ibm.com/ibm/ginni/images/ginni_bio_780x981_v4_03162016.jpg'), indent=2))
+print(json.dumps(visual_recognition.list_classifiers(), indent=2))
+
+with open('/home/kaaappo/Desktop/testi.jpg', 'rb') as image:
+	response = visual_recognition.classify(params, images_file=image)
+print(str(response))
