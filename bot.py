0f3dbcf85820fe3bec2b5c822051dfc4784e8b80
intoalmiala <into.almiala@gmail.com>
2017-11-24 14:36:36 +0200

    optimised general stuff
    e.g. doesn't call watson() six times in an iteration any more and now uses a better getUpdates system

--- bot.py
+++ bot.py
@@ -9,11 +9,6 @@ import urllib
 
 
 # Authentication stuff
-telegram_bot_token= ''
-watson_v_r_token = ''
-watson_nlc_password = ''
-watson_nlc_username = ''
-watson_nlc_id = ''
 telegram_bot_token= '386957960:AAEWqf1iFMjnHk7yJfqK9pHVuWiTaxQpJ1I'
 watson_v_r_token = 'd51997e99c32eeea53de579fa1337829f6c1c3b8'
 watson_nlc_password = 'mawh434bmDVG'
@@ -21,6 +16,8 @@ watson_nlc_username = '5b314083-6286-4ad6-86b0-c6d0ea4aa266'
 watson_nlc_id = 'c533b1x244-nlc-15670'
 
 
+lxybot = telegram.Bot(telegram_bot_token) # initializing a telegram bot instance.
+URL = "https://api.telegram.org/bot{}/".format(telegram_bot_token) # the base URL of the bot api.
 
 # Creating some directories if they do not exist.
 try:
@@ -33,8 +30,6 @@ if not os.path.isdir(os.environ['HOME']  + '/laksybot/temp'): # ./temp is used t
 if not os.path.isdir(os.environ['HOME']  + '/laksybot/ryhmät'): # ... after that, they are moved into ./ryhmät dir.
     os.mkdir(os.environ['HOME']  + '/laksybot/ryhmät')
 
-lxybot = telegram.Bot(telegram_bot_token) # initializing a telegram bot instance.
-URL = "https://api.telegram.org/bot{}/".format(telegram_bot_token) # the base URL of the bot api.
 
 def visual_recognition(path):
     """
@@ -173,50 +168,13 @@ def jsonFromUrl(url):
     js = json.loads(content)
     return js
 
-update_counter = 0 # a counter for getting updates
 
-def getUpdates(): 
-    """
-    Desc:
-        Returns the contents of a 'getUpdates' Telegram API call (as a dictionary), which is
-        a JSON object representing the events that occur during interacton with the bot.
-    Takes:
-        None
-    Returns:
-        dict js : The JSON object representing the events that occur during interaction with the bot
-    Note:
-        If there are more than 90 entries in the JSON object, getUpdatesWithOffset() is called instead.
-    Raises:
-        None
-    """
-    global update_counter
-    def getUpdatesWithOffset(offset=None):
-        """
-        Desc:
-            Does the same as getUpdates()except with an offset, which means that it removes
-            the old updates from the JSON object so that the limit (100) is not exceeded.
-        Takes:
-            int offset (optional, default: None)   : The offset for the API call, meaning the last update id that will be saved
-        Returns:
-            dict js : The JSON object representing the events that occur during interaction with the bot
-        Note:
-            Is not designed to work with other functions.
-        Raises:
-            None
-        """
-        url = URL + "getUpdates?timeout=100&alllowed_updates=['message']&offset={}".format(offset)
-        js = jsonFromUrl(url)
-        return js
+def getUpdates(offset=None): 
+    url = URL + 'getUpdates'
+    if offset:
+        url += '?offset={}'.format(offset)
+    return jsonFromUrl(url)
 
-    url = URL + "getUpdates?timeout=100&alllowed_updates=['message']"
-    if update_counter > 90:
-        update_counter = 0
-        return getUpdatesWithOffset(getLastUpdateId(getUpdatesWithOffset()))
-    js = jsonFromUrl(url)
-    update_counter += 1
-    return js
-
-    
 def lastChatIdText(updates): 
     """
     Desc:
@@ -303,26 +261,9 @@ def getLastUpdateId(updates):
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
 
-def getLastUpdate(updates): 
-    """
-    Desc:
-        Iterates in the getUpdates() JSON object and returns the last update available.
-    Takes:
-        JSON updates : A JSON object of the same form as that is returned from the getUpdates() function.
-    Returns:
-        JSON i : A JSON object representing the last update.
-    Note:
-        Nonw
-    Raises:
-        Exception('No messages yet') if the updates is empty.
-    """
-    update_ids = []
-    if len(updates['result']) == 0:
-        raise Exception('No messages yet')
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
+def getLastUpdate(updates):
     for i in updates['result']:
-        if i['update_id'] == max(update_ids):
+        if i['update_id'] == getLastUpdateId(updates):
             return i
 
 def getFile(file_id, path):
@@ -399,7 +340,7 @@ def getMessageType(dictionary):
     elif 'text' in dictionary:
         return 'text'
 
-def sendImage(chat_id, path, caption=''): 
+def sendImage(chat_id, path, updates, caption=''): 
     """
     Desc:
         Sends an image to a user.
@@ -420,7 +361,7 @@ def sendImage(chat_id, path, caption=''):
         kouluaine_eroteltu = path.split('/')
         kouluaine = kouluaine_eroteltu[len(kouluaine_eroteltu) - 1]
         kouluaine = kouluaine.split('.')[0]
-        sendMessage('Et ole kertonut minulle aineen {} läksyjä!'.format(kouluaine.lower()), lastSenderId(getLastUpdate(getUpdates())))
+        sendMessage('Et ole kertonut minulle aineen {} läksyjä!'.format(kouluaine.lower()), lastSenderId(getLastUpdate(updates)))
     
 def getChatTitle(update): 
     """
@@ -441,77 +382,85 @@ def getChatTitle(update):
         return update['chat']['first_name']
     else:
         raise Exception('chatin titleä ei löytynyt')
+
+def getMessageStuff(last_message):
+    try:
+        last_message_type = getMessageType(last_message['message'])
+        last_message_content = last_message['message']
+    except KeyError:
+        try:
+            last_message_type = getMessageType(last_message['edited_message'])
+            last_message_content = last_message['edited_message']
+        except:
+            raise Exception('Not a message.')
+    return (last_message_type, last_message_content)
+
+
+
     
 def main():
-    last_message_before = None #Initializing a variable before the main loop.
+    last_update_id = None
     while True: # The super-duper-hyper-ultra-extra master champion aka main loop
-        last_message = getLastUpdate(getUpdates()) # The last "message" value in the getUpdates JSON object.
-        try:
-            last_message_type = getMessageType(last_message['message'])
-            last_message_content = last_message['message']
-        except KeyError:
-            try:
-                last_message_type = getMessageType(last_message['edited_message'])
-                last_message_content = last_message['edited_message']
-            except:
-                raise Exception('Not a message.')
-
-        if last_message != last_message_before: # All this happens if somethig new has happened since last update.  
+        updates = getUpdates(last_update_id)
+        if len(updates['result']) > 0:
+            last_message = getLastUpdate(updates) # The last "message" value in the getUpdates JSON object.
+            last_message_type, last_message_content = getMessageStuff(last_message)
+            last_update_id = getLastUpdateId(updates) + 1
             if getMessageType(last_message['message']) != 'photo':
-                chat_id = lastChatIdText(getUpdates())[1]
+                chat_id = lastChatIdText(updates)[1]
             else:
                 continue
             last_title = getChatTitle(last_message_content)
             print('Uusi viesti')
+            if last_message_type == 'text':
+                parsed_text = watson(last_message_content['text'])
+            elif last_message_type == 'caption':
+                parsed_text = watson(last_message_content['caption'])
             try:
                 print('Lähettäjä: {}; Viesti: {}; chat_id: {}'.format(last_title, last_message_content['caption'], chat_id))
-                print(watson(last_message_content['caption']))
+                print(parsed_text)
             except:
                 try:
                     print('Lähettäjä: {}; Viesti: {}; chat_id: {}'.format(last_title, last_message_content['text'], chat_id))
-                    print(watson(last_message_content['text']))
+                    print(parsed_text)
                 except:
                     print('virhe')
                     pass
             
-            chat_id = lastChatIdText(getUpdates())[1]
-            if last_message_type == 'text' and len(last_message_content['text']) < 1024 and watson(last_message_content['text']) != None: # How text messages are treated.
-                
-                kouluaine = watson(last_message_content['text'])
+            chat_id = lastChatIdText(updates)[1]
+            if last_message_type == 'text' and len(last_message_content['text']) < 1024 and parsed_text != None: # How text messages are treated.
+                 
                 path = os.environ['HOME'] + '/laksybot/ryhmät/{}/'.format(chat_id)
                 
                 if not os.path.isdir(path):
                     os.mkdir(path)
-                path += kouluaine + '.jpg'
+                path += parsed_text + '.jpg'
                 print(path)
-                sendImage(chat_id, path, 'Tässä on aineen {} läksy.'.format(kouluaine.lower()))
+                sendImage(chat_id, path, updates, 'Tässä on aineen {} läksy.'.format(parsed_text.lower()))
                 
-            elif last_message_type == 'caption' and len(last_message_content['caption']) < 1024 and watson(last_message_content['caption']) != None: # How images are treated.
-                if len(last_message_content['caption']) > 1024:
+            elif last_message_type == 'caption' and len(last_message_content['caption']) < 1024 and parsed_text != None: # How images are treated.
+                if len(last_message_content['caption']) >= 1024:
                     continue
                 caption = last_message['message']['caption']
                 print('Sain kuvan, jonka käpsöni oli:', caption)
-                kouluaine = watson(caption)
                 if not os.path.isdir('./ryhmät/{}'.format(chat_id)):
                     os.mkdir('./ryhmät/{}'.format(chat_id))
                 if not os.path.isdir('./temp/{}'.format(chat_id)):
                     os.mkdir('./temp/{}'.format(chat_id))
-                if os.path.isfile('./temp/{}/{}.jpg'.format(chat_id, kouluaine)):
-                    os.remove('./temp/{}/{}.jpg'.format(chat_id, kouluaine))
-                getFile(getFileId(1), 'temp/{}/{}.jpg'.format(chat_id, kouluaine))
-                print(visual_recognition('./temp/{}/{}.jpg'.format(chat_id, kouluaine)))
-                if visual_recognition('./temp/{}/{}.jpg'.format(chat_id, kouluaine)) == 'liitutaulu':
-                    if os.path.isfile('./ryhmät/{}/{}.jpg'.format(chat_id, kouluaine)):
-                        os.remove('./ryhmät/{}/{}.jpg'.format(chat_id, kouluaine))
-                    os.rename('./temp/{}/{}.jpg'.format(chat_id, kouluaine), './ryhmät/{}/{}.jpg'.format(chat_id, kouluaine))
-                    sendMessage('Selvä! Muistan nyt aineen {} läksyn!'.format(kouluaine.lower()), chat_id)
+                if os.path.isfile('./temp/{}/{}.jpg'.format(chat_id, parsed_text)):
+                    os.remove('./temp/{}/{}.jpg'.format(chat_id, parsed_text))
+                getFile(getFileId(1), 'temp/{}/{}.jpg'.format(chat_id, parsed_text))
+                print(visual_recognition('./temp/{}/{}.jpg'.format(chat_id, parsed_text)))
+                if visual_recognition('./temp/{}/{}.jpg'.format(chat_id, parsed_text)) == 'liitutaulu':
+                    if os.path.isfile('./ryhmät/{}/{}.jpg'.format(chat_id, parsed_text)):
+                        os.remove('./ryhmät/{}/{}.jpg'.format(chat_id, parsed_text))
+                    os.rename('./temp/{}/{}.jpg'.format(chat_id, parsed_text), './ryhmät/{}/{}.jpg'.format(chat_id, parsed_text))
+                    sendMessage('Selvä! Muistan nyt aineen {} läksyn!'.format(parsed_text.lower()), chat_id)
                 else:
                     sendMessage('Hyvä yritys, mutta tuolla kuvalla ei ole mitään tekemistä läksyjen kanssa...', chat_id)
                     print('Ei ole liitutaulu.')
                 print('\nKuva ladattu onnistuneesti')
                 
-        last_message_before = last_message
-    last_message_before = last_message # Updating the last_message_before var.
     
 if __name__ == '__main__': # If a foreign script calls this file, it still works.
     main()
